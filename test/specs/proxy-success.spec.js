45d86db8289169c618ea80aba0f464148f9790c1
vitalets <vitalets@yandex-team.ru>
2020-04-28 15:23:47 +0300

    test: add skipped redirect test

--- test/specs/proxy-success.spec.js
+++ test/specs/proxy-success.spec.js
@@ -11,6 +11,23 @@ describe('proxy success', () => {
     assert.deepEqual(response, { response: { text: 'bar' } });
   });
 
+  // todo: do manual redirection, because POST is not redirected automatically
+  // see: https://github.com/node-fetch/node-fetch#responseredirected
+  it.skip('return response with redirect', async () => {
+    const scope = nock('http://localhost')
+      .post('/')
+      .reply(302, undefined, {
+        'Location': 'http://localhost/skill'
+      })
+      .post('/skill')
+      .reply(200, { response: { text: 'bar' } });
+
+    const response = await callFn({ request: { command: 'foo' } });
+
+    scope.done();
+    assert.deepEqual(response, { response: { text: 'bar' } });
+  });
+
   it('incorrect request format: proxy to target anyway', async () => {
     const scope = nock('http://localhost')
       .post('/')
